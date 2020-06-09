6e9543f915f73ed70c00073068d335ca39d87226
kangdari_ <ksh44820@naver.com>
2020-06-09 20:18:51 +0900

    현재 활성화시킬 씬을 결정하고 반영 load, scroll 이벤트 발생 시 현재 활성화 씬을 결정하여 body에   id 값을 설정하고 이에 따라 css 적용

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 // 즉시 실행 함수
 // 전역 변수 사용을 피하기 위해서
 (() => {
+  let yOffset = 0; // window.pageYOffset: 현재 스크롤 위치
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 scrollHeight 합.
+  let currentScene = 0; // 현재 보여지는 씬  = scroll_section
+
   const sceneInfo = [
     // 4개의 section 정보
     {
@@ -53,12 +57,60 @@
         i
       ].obj.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
-    console.log(sceneInfo);
+
+    yOffset = window.pageYOffset;
+    // 첫 로드 되었을 때 스크롤이 위치한 현재 씬을 계산
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      // 현재 씬 이전 까지의 스크롤 높이 합 >= 현재 스크롤 위치
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show_scene_0${currentScene + 1}`);
+  };
+
+  // 활성화 시킬 scene의 번호 설정
+  const scrollLoop = () => {
+    prevScrollHeight = 0;
+    // 현재 씬 이전의 씬들의 스크롤 높이 합 계산.
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    // 현재 스크롤 위치 > 이전 씬들의 스크롤 높이 합(현재씬-1 개) + 현재 씬의 스크롤 높이
+    // 즉 섹션이 바뀔 때...
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+      document.body.setAttribute("id", `show_scene_0${currentScene + 1}`);
+    }
+    // 현재 스크롤 위치 < 이전 씬들의 스크롤 높이 합
+    // 섹션이 바뀔 때
+    if (yOffset < prevScrollHeight) {
+      // currentScene이 -1 되는 경우를 방지
+      if (currentScene === 0) return;
+      currentScene--;
+      document.body.setAttribute("id", `show_scene_0${currentScene + 1}`);
+    }
+
+    // body 요소에 현재 씬 관련 아이디 값을 설정하면
+    // 씬에 해당하는 .stick_elem 요소가 보임
+    // document.body.setAttribute("id", `show_scene_0${currentScene + 1}`);
   };
 
   // 브라우저의 사이즈가 변경되면 section의 높이를 재설정
   window.addEventListener("resize", setLayout);
-  setLayout();
+  window.addEventListener("scroll", () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
 
-  //
+  // DOMContentLoaded: html 요소들으 로드될 때
+  // window.addEventListener("DOMContentLoaded", setLayout);
+
+  // load: html요소들과 img와 같은 리소스들도 로드될 때
+  // 문서가 로드되면 setLayout 실행.
+  window.addEventListener("load", setLayout);
 })();
